785f8ef825970091c0753073f99913805ee06dc0
S.Vel <85781428+S-Vel@users.noreply.github.com>
2021-07-12 21:20:14 +0530

    Add files via upload

---
+++ Q1.py
@@ -0,0 +1,5 @@
+#1. Enumerate a python list and try to print the counter with the list value
+
+L = ["Old", "Is", "Gold"]
+counterL = list(enumerate(L, 1))
+print(counterL)
---
+++ Q2.py
@@ -0,0 +1,5 @@
+#2. Enumerate a python tuple and try to print the counter with the tuple value
+
+T = (29, 30, 31, 32, 33, 34, 35, 36, 37, 38)
+counterT = tuple(enumerate(T, 1))
+print(counterT)
